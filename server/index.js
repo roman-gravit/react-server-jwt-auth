b9c1e5e31b8871876d500421ca2dd4aa23f5d44d
Roman Gravit <romang@siber.com>
2024-04-21 17:14:36 +0100

    Connect to DB, ad middlewares

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-
+const db_connection_url = process.env.MONGODB_URL || "";
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 const start = async() => {
 
 	try {
+		await mongoose.connect(db_connection_url);
 		app.listen(PORT, () => { console.log(`server listening on port:${PORT}`);})
 
 	} catch(e) {
